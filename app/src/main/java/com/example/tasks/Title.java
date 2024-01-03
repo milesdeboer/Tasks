0d62e7b7ae98c5c7e7e8bea458973964f8c53c34
Miles DeBoer <miles.deboer.p@gmail.com>
2024-01-02 18:13:28 -0800

    functional button connecting title to main activity

--- app/src/main/java/com/example/tasks/Title.java
+++ app/src/main/java/com/example/tasks/Title.java
@@ -2,7 +2,10 @@ package com.example.tasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Title extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Title extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        Button button = findViewById(R.id.start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Title.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
